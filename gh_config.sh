054211fd17bd4b7746662eb1f228cca42e150426
Ryandito Diandaru <ryanditodua@gmail.com>
2025-04-20 07:07:45 +0000

    cuda dot product

---
+++ gh_config.sh
@@ -0,0 +1,2 @@
+git config --global user.email "ryanditodua@gmail.com"
+git config --global user.name "Ryandito Diandaru"
--- src/dot.cu
+++ src/dot.cu
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#define N 10
-#define HEAD 2
-#define LD 32
+#define N 10000
+#define LD 16
 const int threadsPerBlock = LD * LD;
 
 #define HANDLE_ERROR(ans) { gpuAssert((ans), __FILE__, __LINE__); }
@@ -17,31 +16,45 @@ __global__ void kernel(int* a, int* b, int* c_partial){
     int flatBlockIdx = blockIdx.x + blockIdx.y * gridDim.x;
     int flatThreadIdx = threadIdx.x + threadIdx.y * blockDim.x;
     int idx = flatThreadIdx + flatBlockIdx * blockDim.x * blockDim.y;
+    
     float temp = 0;
-
     if(idx < N){
-        temp = temp + a[idx] * b[idx];
-        idx = idx + blockDim.x * blockDim.y * gridDim.x * gridDim.y;
+        temp += a[idx] * b[idx];
+        idx += blockDim.x * blockDim.y * gridDim.x * gridDim.y;
     }
 
-    if (blockIdx.x == 0 && blockIdx.y == 0 && threadIdx.x == 1 && threadIdx.y == 0){
-        printf("%d - %d\n", flatThreadIdx, temp);
-    }
-
+    // writing to cache can be done independently bc each thread has its own space to write
     cache[flatThreadIdx] = temp;
+
+    // yg perlu ditunggu tuh kalo udah beres write to cache semua
     __syncthreads();
 
-    // if (blockIdx.x == 0 && blockIdx.y == 0 && threadIdx.x == 0 && threadIdx.y == 0) {
-        // printf("%d - %d\n", flatThreadIdx, temp);
-        // for(int i = 0; i < threadsPerBlock; i++){
-        //     printf("%d ", cache[i]);
+    // now, reduce operation.
+    // array cache tadi dilipet2 ampe jadi satu (definisi lipet: diadd biasa aja)    
+    int middle = threadsPerBlock/2;
+    while (middle != 0) {
+        if(flatThreadIdx < middle){
+            cache[flatThreadIdx] += cache[flatThreadIdx+middle];
+        }
+        __syncthreads(); // make sure every thread has ngelipet their portion already
+        
+        // uncomment if u wanna see the addition reduce process wkwk
+        // if (blockIdx.x == 0 && blockIdx.y == 0 && threadIdx.x == 0 && threadIdx.y == 0) {
+        //     for(int i = 0; i < middle ; i++){
+        //         printf("%.0f ", cache[i]);
+        //     }
+        //     printf("\n");
         // }
-    // }
 
-    // while (idx < N){
-    //     cache[flatThreadIdx] = a[idx] * b[idx];
-    //     idx = idx + blockDim.x * blockDim.y * gridDim.x * gridDim.y;
-    // }
+        middle /= 2;
+    }
+
+    // now put the cache[0] (result of the reduce, local to the block)
+    // in the c_partial that that's the size of the number of blocks that we have
+    // satu thread aja yang ngelakuin wkoakwawok
+    if (flatThreadIdx == 0) {
+        c_partial[flatBlockIdx] = cache[0];
+    }
 }
 
 int main(void){
@@ -59,7 +72,6 @@ int main(void){
     // malloc biasa ygy
     a = (int*) malloc(sizeof(int) * N);
     b = (int*) malloc(sizeof(int) * N);
-    
     // cache perlu dialloc ga kalo gitu?
     // cache (and dev_c_partial) will be an array yg masih perlu disum (hasil dari tiap block)
     // but it's gonna be small enough that it's reasonable to compute sequentially
@@ -78,15 +90,29 @@ int main(void){
     HANDLE_ERROR(cudaMalloc((void**) &dev_b, sizeof(int) * N));
     HANDLE_ERROR(cudaMalloc((void**) &dev_c_partial, sizeof(int) * totalBlocks));
 
-    // copy to device
+    // copy input to device
     HANDLE_ERROR(cudaMemcpy(dev_a, a, sizeof(int) * N, cudaMemcpyHostToDevice));
     HANDLE_ERROR(cudaMemcpy(dev_b, b, sizeof(int) * N, cudaMemcpyHostToDevice));
 
     kernel<<<numBlocks, threadsPerBlock>>>(dev_a, dev_b, dev_c_partial);
 
+    // copy output back to host
+    HANDLE_ERROR(cudaMemcpy(c_partial, dev_c_partial, sizeof(int) * totalBlocks, cudaMemcpyDeviceToHost));
+
+    float c = 0;
+    for(int i = 0; i < totalBlocks; i++){
+        c += c_partial[i];
+    }
+
+    printf("dot product : %.2f\n", c);
+
     HANDLE_ERROR(cudaFree(dev_a));
     HANDLE_ERROR(cudaFree(dev_b));
     HANDLE_ERROR(cudaFree(dev_c_partial));
+
+    free(a);
+    free(b);
+    free(c_partial);
 
     return 0;
 }
